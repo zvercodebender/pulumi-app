525d96fcfa5fbf8d447a266e0188bde630c7c076
rick <rick.broker@gmail.com>
2025-02-06 14:24:46 -0500

    Add CI

--- ci.sh
+++ ci.sh
@@ -1,3 +1,17 @@
-docker build -t rbroker/pulumi-lab:latest .
-docker push rbroker/pulumi-lab:latest
+export REPO="rbroker/pulumi-lab"
+TAG="$(git rev-parse --short HEAD)"
+#export VERSION="3.1.1"
+if [ -z "${TAG}" ]
+then
+  echo "VERSION not set"
+  TAG=latest
+fi
+echo "Version = ${TAG}"
+IMAGE="${REPO}:${TAG}"
+
+echo "docker build -t ${IMAGE} -f Dockerfile ."
+docker build -t ${IMAGE}:${TAG} .
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD docker.io
+echo "docker push ${IMAGE}"
+docker push ${IMAGE}
 #docker-compose up -d
