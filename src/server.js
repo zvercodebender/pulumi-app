5d7a5a19bd733ee97fe9c3e37e2eb8a4f7c6cc85
rick <rick.broker@gmail.com>
2025-02-26 09:53:04 -0500

    Make env variable whole message

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello ' + process.env.MESSAGE);
+  res.send(process.env.MESSAGE);
 });
 
 app.listen(PORT, HOST);
